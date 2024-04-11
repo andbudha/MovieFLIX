aa45a3ef2428b3abcc41a34637449f48e27e75fe
andbudha <andreibartov@gmail.com>
2024-04-11 14:59:55 +0200

    backend data-getting-func-ty updated

--- api.js
+++ api.js
@@ -1,14 +1,10 @@
 const url = 'https://5b81e3264853b358.mokky.dev/cabmovies';
 
 const getMovies = async () => {
-  try {
-    const response = await fetch(url);
-    const result = await response.text();
-    const data = JSON.parse(result);
-    console.log(data.slice(0, 8));
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await fetch(url);
+  const data = await response.json();
+  createMovieCard(data.slice(1, 6));
+  return data;
 };
-const movies = getMovies();
-console.log(movies);
+
+console.log(getMovies());
